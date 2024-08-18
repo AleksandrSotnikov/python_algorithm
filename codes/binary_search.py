c60055f4890761aaa23bbe2ff0c8849886b2704f
Alexandr Sotnikov <sotnikovalex143@gmail.com>
2024-08-18 21:46:55 +0600

    Refactor: Implement binary search with count

--- codes/binary_search.py
+++ codes/binary_search.py
@@ -1,15 +1,30 @@
 def binary_search(arr, target):
     low = 0
-    high = len(arr)-1
+    high = len(arr) - 1
     while low <= high:
-        mid = (low+high)
+        mid = (low + high)//2
         res = arr[mid]
         if target == res:
             return mid
         if target < res:
-            high = mid-1
+            high = mid - 1
         else:
-            low = mid+1
+            low = mid + 1
     return None
 
 
+def binary_search_count_operation(arr, target):
+    low = 0
+    high = len(arr) - 1
+    count = 0
+    while low <= high:
+        count += 1
+        mid = (low + high)//2
+        res = arr[mid]
+        if target == res:
+            return count
+        if target < res:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return count
